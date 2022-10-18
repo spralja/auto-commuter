bd78e112264370a68f335317658a70ecff1ebd8c
Robert Spralja <56681655+spralja@users.noreply.github.com>
2022-10-18 18:54:14 +0200

    Added rejseplanen api integration

---
+++ rejseplanen.js
@@ -0,0 +1,93 @@
+
+
+class RejseplanenClient {
+    constructor(base_url, xml) {
+        this.base_url = base_url;
+        this.format = (xml ? '' : '&format=json');
+    }
+
+    request(service, options) {
+        let options_array = [];
+        for(const [key, value] of Object.entries(options)) {
+            options_array.push(`${key}=${value}`);
+        }
+
+        let options_string = options_array.join('&');
+
+        let url = `${this.base_url}/${service}?${options_string}${this.format}`
+        print(url);
+        let xmlHttp = new XMLHttpRequest();
+        xmlHttp.open('GET', url, false);
+        xmlHttp.send(null);
+        return JSON.parse(xmlHttp.responseText);
+    }
+
+    location(input) {
+        let service = 'location';
+    }
+
+    trip(options) {
+        let service = 'trip'
+        /*
+         required:
+         originId || originCoordX, originCoordY, originCoordName;
+         destId || destCoordX, destCoordY, destCoordName
+
+         optional:
+         viaId, date=now, time=now, searchForArrival=0
+         useTog=1, useBus=1, useMetro=1
+         useBicycle=1 ~=> maxWalkingDistanceDep=2000, maxWalkingDistanceDest=2000
+        */
+
+
+    }
+
+    departureBoard() {
+        let service = 'departureBoard';
+        /*
+         required:
+         id; date, time || offsetTime (minutes)
+
+         optional
+         useTog, useBus, useMetro
+
+         */
+    }
+
+    arrivalBoard() {
+        let service = 'arrivalBoard';
+        /*
+         required:
+         id; date, time || offsetTime (minutes)
+
+         optional:
+         useTog, useBus, useMetro
+
+         */
+    }
+
+    multiDepartureBoard() {
+        let service = 'multiDepartureBoard';
+        /*
+         required:
+         id1, [id2, id3, ..., id10]; date, time || offsetTime (minutes0
+
+         optional:
+         useTog, useBus, useMetro
+         */
+    }
+
+    stopsNearby() {
+        let service = 'stopsNearby';
+        /*
+         required:
+         coordX, coordY, maxRadius, maxNumber
+         */
+    }
+
+    journeyDetail() {
+        let service = 'journeyDetail';
+        //TODO
+    }
+
+}
--- sketch.js
+++ sketch.js
@@ -1,34 +1,82 @@
-let webcals_prompt, button, greeting;
+let departure_text, departure_prompt, departure_button;
+let destination_text, destination_prompt, destination_button;
+
+let departure_location_x, departure_location_y;
+let destination_location_x, destination_location_y;
+
+let input;
+
+let rejseplanen_client = new RejseplanenClient('http://xmlopen.rejseplanen.dk/bin/rest.exe');
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
 
-  webcals_prompt = createInput();
-  webcals_prompt.position(20, 65);
-
-  button = createButton('submit');
-  button.position(webcals_prompt.x + webcals_prompt.width, 65);
-  button.mousePressed(greet);
-
-  greeting = createElement('h2', 'Course webcal files:');
-  greeting.position(20, 5);
-
-  textAlign(CENTER);
-  textSize(50);
-
   input = createFileInput(handleFile);
   input.position(0, 0);
+
+  departure_text = createElement('h4', 'Departure:')
+  departure_text.position(10, 100);
+
+  departure_prompt = createInput();
+  departure_prompt.position(departure_text.x + 90, 120);
+  departure_prompt.size(400, departure_prompt.height)
+
+  departure_button = createButton('submit');
+  departure_button.position(departure_prompt.x + departure_prompt.width, 120);
+  departure_button.size(departure_button.width, departure_prompt.height);
+  departure_button.mousePressed(submit_departure);
+
+  destination_text = createElement('h4', 'Destination:');
+  destination_text.position(10, 130);
+
+  destination_prompt = createInput();
+  destination_prompt.position(destination_text.x + 90, 150)
+  destination_prompt.size(400, destination_prompt.height)
+
+  destination_button = createButton('submit');
+  destination_button.position(destination_prompt.x + destination_prompt.width, destination_prompt.y);
+  destination_button.size(destination_button.width, destination_prompt.height);
+  destination_button.mousePressed(destination_submit);
+
+}
+
+function submit_departure() {
+  let departure = departure_prompt.value();
+  let response = rejseplanen_client.request(
+      'location',
+      {'input': departure}
+  )
+
+  let location = response['LocationList']['CoordLocation'][0];
+
+  departure_prompt.value(location.name);
+
+  departure_location_x = location.x;
+  departure_location_y = location.y;
+
+  print(departure_location_x);
+  print(departure_location_y);
+}
+
+function destination_submit() {
+  let destination = destination_prompt.value();
+  let response = rejseplanen_client.request(
+      'location',
+      {'input': destination}
+  )
+
+  let location = response['LocationList']['CoordLocation'][0];
+
+  departure_prompt.value(location.name);
+
+  destination_location_x = location.x;
+  destination_location_y = location.y;
+
+  print(destination_location_x);
+  print(destination_location_y);
 }
 
 let ical;
-
-function greet() {
-  webcals = webcals_prompt.value().split(';')
-  print(webcals);
-  httpGet(webcals[0], 'text', false, function(response) {
-    ical = response;
-  });
-}
 
 function draw() {
   if (ical === undefined) return;
@@ -39,5 +87,23 @@ function draw() {
 function handleFile(file) {
   let calendar = new Calendar(file.data);
   print(calendar);
+  let client = new RejseplanenClient('http://xmlopen.rejseplanen.dk/bin/rest.exe');
+  let rtext = client.request('location', {input: 'Trekroner'});
+  print(rtext);
 
+  if(!navigator.geolocation) {
+    console.log('Geolocation is not supported by your browser');
+
+  } else {
+    navigator.geolocation.getCurrentPosition(success, error)
+  }
+}
+
+function success(position) {
+  print(position.coords.latitude);
+  print(position.coords.longitude);
+}
+
+function error() {
+  print('Unable to retrive your location');
 }
