631a636cff09cad20443b998b9ec52083e76f0fd
Alona Konstantinova <alona@ruc.dk>
2022-11-24 00:05:33 +0100

    fixes:dropdown works for departure and destination; when location is selected from dropdown it is displayed in the prompt and the dropdown is removed; it appears again if user uses prompt again.

--- sketch.js
+++ sketch.js
@@ -4,6 +4,8 @@ let destination_text, destination_prompt, destination_button;
 let departure_location_x, departure_location_y, departure_location_name;
 let destination_location_x, destination_location_y, destination_location_name;
 
+let departure_picker,destination_picker
+
 let generate_calendar_button;
 
 let input;
@@ -12,8 +14,14 @@ let rejseplanen_client = new RejseplanenClient('https://xmlopen.rejseplanen.dk/b
 
 let calendar;
 
-var lastRequestTime = 0;
-var lastRequestedInput = "";
+var lastRequestTime_departure = 0;
+var lastRequestedInput_departure = "";
+
+var lastRequestTime_destination = 0;
+var lastRequestedInput_destination = "";
+
+var picked_departure = false;
+var picked_destination = false;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -24,8 +32,8 @@ function setup() {
   departure_text = createElement('h4', 'Departure:')
   departure_text.position(10, 100);
 
-
-  departure_prompt = createInput().attribute('placeholder', 'ex. Roskilde St.');
+  textStyle(ITALIC);
+  departure_prompt = createInput().attribute('placeholder','ex. Roskilde St.');
   departure_prompt.position(departure_text.x, departure_text.y + 40);
   departure_prompt.size(200, departure_prompt.height);
 
@@ -37,14 +45,9 @@ function setup() {
   destination_text = createElement('h4', 'Destination:');
   destination_text.position(departure_text.x + 600, 100);
 
-  destination_prompt = createInput();
+  destination_prompt = createInput().attribute('placeholder','ex. RUC');
   destination_prompt.position(destination_text.x, destination_text.y + 40)
   destination_prompt.size(200, destination_prompt.height);
-  //destination_prompt.input(suggest_destination);
-
-  //destination_picker = createSelect();
-  //destination_picker.position(destination_prompt.x,destination_prompt.y+40,);
-  //destination_picker.changed(select_destination);
 
 
   destination_button = createButton('submit');
@@ -61,27 +64,37 @@ function setup() {
 
 function draw() {
 
-  departure_prompt.input(time_millis);
-  suggest_departure();
-  print(lastRequestTime);
+ departure_prompt.input(input_departure);
+ suggest_departure();
+
+
+ destination_prompt.input(input_destination);
+ suggest_destination();
 
   if (ical === undefined) return;
 
   //print(ical);
 }
 
-function time_millis(){
-  lastRequestTime = millis();
+function input_departure(){
+  lastRequestTime_departure = millis();
+  picked_departure = false;
+}
+
+function input_destination(){
+  lastRequestTime_destination = millis();
+  picked_destination = false;
 }
 
 function suggest_departure() {
   let initialInput = departure_prompt.value();
-  if (millis() - lastRequestTime > 2000 && initialInput != lastRequestedInput) {
-    if (lastRequestedInput != "") {
+  if (millis() - lastRequestTime_departure > 1000 && initialInput != lastRequestedInput_departure && (picked_departure==false)) {
+
+    if (lastRequestedInput_departure != "") {
       departure_picker.remove();
     }
-    lastRequestedInput = departure_prompt.value;
-    lastRequestedInput = initialInput;
+    lastRequestedInput_departure = departure_prompt.value;
+    lastRequestedInput_departure = initialInput;
 
     if (initialInput == 0) {
       departure_picker.remove();
@@ -93,40 +106,66 @@ function suggest_departure() {
 
       departure_picker = createSelect();
       departure_picker.position(departure_prompt.x, departure_prompt.y + 25);
+      departure_picker.size(departure_prompt.width, 25)
       var i = 0;
       while (i < 5) {
         departure_picker.option(location[i].name);
         i++;
       }
+      departure_picker.changed(select_departure);
     }
   }
 }
 
+function suggest_destination() {
+  let initialInput = destination_prompt.value();
+  if (millis() - lastRequestTime_destination > 1000 && initialInput != lastRequestedInput_destination && (picked_destination == false)) {
+
+    if (lastRequestedInput_destination != "") {
+      destination_picker.remove();
+    }
+    lastRequestedInput_destination = destination_prompt.value;
+    lastRequestedInput_destination = initialInput;
+
+    if (initialInput == 0) {
+      destination_picker.remove();
+    } else {
+      let response = rejseplanen_client.location(initialInput);
+      let location_stops = response["LocationList"]["StopLocation"];
+      let location_coors = response["LocationList"]["CoordLocation"];
+      let location = location_stops.concat(location_coors);
+
+      destination_picker = createSelect();
+      destination_picker.position(destination_prompt.x,destination_prompt.y+25);
+      destination_picker.size(destination_prompt.width, 25);
+
+      var i = 0;
+      while (i < 5) {
+        destination_picker.option(location[i].name);
+        i++;
+      }
+      destination_picker.changed(select_destination);
+    }
+  }
+}
 
 
 //function to display a selected location from the dropdown in the prompt
 function select_departure(){
   let selected = departure_picker.selected();
   departure_prompt.value(selected);
-}
-
-function suggest_destination() {
-  let destination = destination_prompt.value();
-  let response = rejseplanen_client.location(destination);
-  let location_stops = response['LocationList']['StopLocation']
-  let location_coors = response['LocationList']['CoordLocation'];
-  let location = location_stops.concat(location_coors);
-  var i = 0;
-  while (i < 30) {
-    destination_picker.option(location[i].name);
-    i++;
-  }
+  picked_departure = true;
+  departure_picker.remove();
 }
 
 function select_destination(){
   let selected = destination_picker.selected();
   destination_prompt.value(selected);
+  picked_destination = true;
+  destination_picker.remove();
 }
+
+
 
 
 function submit_departure() {
@@ -148,12 +187,6 @@ function submit_departure() {
   //print(departure_location_y);
 }
 
-//function removeOptions() {
- // var i, L = departure_picker.options.length - 1;
- // for(i = L; i >= 0; i--) {
-   // departure_picker.remove(i);
-  //}
-//}
 
 function submit_destination() {
   if(!destination_prompt.value()) throw 'Destination field cannot be empty!';
@@ -221,16 +254,6 @@ function generate_calendar() {
 
 let ical;
 
-function draw() {
-
-  departure_prompt.input(time_millis);
-  suggest_departure();
-  print(lastRequestTime);
-
-  if (ical === undefined) return;
-
-  //print(ical);
-}
 
 
 function handleFile(file) {
